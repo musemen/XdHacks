1447fb628cb465efeeb91b9ddc2f650aaa96423a
Musa mohannad <musa.mohannad1@gmail.com>
2019-02-09 23:26:31 -0800

    Frontend

--- app/src/main/java/com/example/xdhacks/MainActivity.java
+++ app/src/main/java/com/example/xdhacks/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.xdhacks;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = (VideoView) findViewById(R.id.bgVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.loginvid);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
     }
+    public void gotoprofile(View v){
+        Intent intent = new Intent(this, newProfile.class);
+        startActivity(intent);
+    }
+
+
 }
